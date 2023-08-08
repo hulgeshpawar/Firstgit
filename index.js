a330edb8f69d58389f3006c9308a79efe7206eb9
Hulgesh Pawar <105061854+hulgeshpawar@users.noreply.github.com>
2023-08-08 16:43:06 +0530

    index

---
+++ index.js
@@ -0,0 +1,28 @@
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.title);
+
+//document.title = 123;
+
+//console.log(document.head);
+//console.log(document.all);
+//console.log(document.all[18]);
+
+//document.all[18].textContent = 'Hello';
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+
+// console.log(document.getElementById('header-title'));
+
+var hader = document.getElementById('header-title');
+//hader.textContent = 'Hello';
+//hader.innerText = 'Goodbye';
+//console.log(hader);
+
+// console.log(hader.innerText);
+hader.style.borderBottom='solid 3px black   '
+
+var header = document.getElementsByClassName('title') ;
+header[0].style.fontWeight='bold';
+header[0].style.color= 'green';
