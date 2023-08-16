e77e6c3832e18cd058795b9f8d3f271385c93939
Hulgesh Pawar <105061854+hulgeshpawar@users.noreply.github.com>
2023-08-16 12:34:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -15,22 +15,30 @@
 
 // console.log(document.getElementById('header-title'));
 
-var hader = document.getElementById('header-title');
+//var hader = document.getElementById('header-title');
 //hader.textContent = 'Hello';
 //hader.innerText = 'Goodbye';
 //console.log(hader);
 
 // console.log(hader.innerText);
-hader.style.borderBottom='solid 3px black   '
+//hader.style.borderBottom='solid 3px black   '
 
-var header = document.getElementsByClassName('title') ;
-header[0].style.fontWeight='bold';
-header[0].style.color= 'green';
+//var header = document.getElementsByClassName('title') ;
+//header[0].style.fontWeight='bold';
+//header[0].style.color= 'green';
 
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor= 'green';
-console.log(items);
+//var items = document.getElementsByClassName('list-group-item');
+//items[2].style.backgroundColor= 'green';
+//console.log(items);
 
-for(i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+//for(i=0;i<items.length;i++){
+//    items[i].style.fontWeight = 'bold';
+//}
+
+
+var li = document.getElementsByTagName('li');
+
+li[1].textContent = 'Hello2';
+for (i=0;i<=li.length;i++){
+    li[i].style.background = 'grey';
 }
